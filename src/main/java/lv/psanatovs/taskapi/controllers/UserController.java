2742a59f1dda9eb3ea763a8ffdd4fefab9846f08
Ma Nul <psanatovs@gmail.com>
2024-04-29 00:13:54 +0300

    Created UserEntity with basic fields. Wired in-memory h2 database.

---
+++ src/main/java/lv/psanatovs/taskapi/controllers/UserController.java
@@ -0,0 +1,20 @@
+package lv.psanatovs.taskapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<?> getUsers() {
+        try {
+            return ResponseEntity.ok("Users: HTTP OK");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Users: HTTP Error!");
+        }
+    }
+}
